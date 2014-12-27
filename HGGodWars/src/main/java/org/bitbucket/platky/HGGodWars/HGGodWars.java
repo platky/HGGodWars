b0b22336eaae4b36fcc90787584a8da97cecb0b7
Kyle <platky1@gmail.com>
2014-12-27 01:56:17 -0500

    Starting

---
+++ HGGodWars/src/main/java/org/bitbucket/platky/HGGodWars/HGGodWars.java
@@ -0,0 +1,14 @@
+package org.bitbucket.platky.HGGodWars;
+
+import org.bukkit.plugin.java.*;
+
+public class HGGodWars {
+
+	public void onEnable() {
+		getLogger().info("onEnable has been invoked");
+	}
+	
+	public void onDisable() {
+		
+	}
+}
